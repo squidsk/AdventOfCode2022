992bda3a18159caf9980e8ccc8421179fc616b17
squidsk <squidsk@gmail.com>
2022-12-05 23:01:54 -0700

    Day 6 Part 1 and Part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -6,6 +6,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day6
 {
@@ -13,9 +14,10 @@ namespace Day6
     {
         public static void Main(string[] args) {
             Part1("test.txt");
-            //Part1("input.txt");
+            Part1("test2.txt");
+            Part1("input.txt");
             //Part2("test.txt");
-            //Part2("input.txt");
+            Part2("input.txt");
 
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
@@ -23,17 +25,41 @@ namespace Day6
 
         static void Part1(string filename) {
             using(StringReader reader = new StringReader(File.ReadAllText(filename))){
-                // TODO: Implement Functionality Here
+    			string line = reader.ReadLine();
+    			string match = line.Substring(0,1);
+    			int positionOfMatch;
+    			
+    			for(positionOfMatch = 1 ; positionOfMatch < line.Length; positionOfMatch += 1){
+    				int index = match.IndexOf(line[positionOfMatch]);
+    				if(index != -1) {
+    					match = match.Substring(index+1) + line[positionOfMatch];
+    				} else {
+    					match += line[positionOfMatch];
+    					if(match.Length == 4) break;
+    				}
+    			}
 
-                Console.WriteLine("The solution to Part 1 with inputfile: {0} is: {1}", filename, "Put solution here!");
+                Console.WriteLine("The start of packet marker in file {0} is completed at character: {1}", filename, positionOfMatch+1);
             }
         }
 
         static void Part2(string filename) {
             using(StringReader reader = new StringReader(File.ReadAllText(filename))){
-                // TODO: Implement Functionality Here
+    			string line = reader.ReadLine();
+    			string match = line.Substring(0,1);
+    			int positionOfMatch;
+    			
+    			for(positionOfMatch = 1 ; positionOfMatch < line.Length; positionOfMatch += 1){
+    				int index = match.IndexOf(line[positionOfMatch]);
+    				if(index != -1) {
+    					match = match.Substring(index+1) + line[positionOfMatch];
+    				} else {
+    					match += line[positionOfMatch];
+    					if(match.Length == 14) break;
+    				}
+    			}
 
-                Console.WriteLine("The solution to Part 2 with inputfile: {0} is: {1}", filename, "Put solution here!");
+                Console.WriteLine("The start of packet marker in file {0} is completed at character: {1}", filename, positionOfMatch+1);
             }
         }
     }
