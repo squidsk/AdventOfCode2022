3b57b2ba444fdb65e54d2e32c8f20541fa6bb28f
squidsk <squidsk@gmail.com>
2022-12-08 01:21:02 -0700

    Setup for Day 8

---
+++ Day8/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mom and Dad
+ * Date: 2022-12-08
+ * Time: 1:13 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day8
+{
+	class Program
+	{
+		public static void Main(string[] args) {
+			Part1("test.txt");
+			//Part1("input.txt");
+			//Part2("test.txt");
+			//Part2("input.txt");
+
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+
+		static void Part1(string filename) {
+			using(StringReader reader = new StringReader(File.ReadAllText(filename))){
+				// TODO: Implement Functionality Here
+
+				Console.WriteLine("The solution to Part 1 with inputfile: {0} is: {1}", filename, "Put solution here!");
+			}
+		}
+
+		static void Part2(string filename) {
+			using(StringReader reader = new StringReader(File.ReadAllText(filename))){
+				// TODO: Implement Functionality Here
+
+				Console.WriteLine("The solution to Part 2 with inputfile: {0} is: {1}", filename, "Put solution here!");
+			}
+		}
+	}
+}
