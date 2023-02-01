511ad6532b02fb5f201a02d6e3f890fcc376fdb0
squidsk <squidsk@gmail.com>
2023-02-01 16:54:19 -0700

    Day 11 Part 1 and Part 2 Complete

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -10,45 +10,61 @@ using System.Collections.Generic;
 namespace Day11
 {
     class Monkey {
-        List<int> items;
+        public static int globalWorryModifier = 1;
+        List<long> items;
         int trueMonkey;
         int falseMonkey;
         int divisor;
         char operation;
         int operand;
-        int inspections;
+        long inspections;
+        long worryModifier;
         
-        public Monkey(int trueMonkey, int falseMonkey, int divisor, char operation, int operand, int[] items) {
-            this.items = new List<int>();
+        public Monkey(int trueMonkey, int falseMonkey, int divisor, char operation, int operand, int worryModifier, long[] items) {
+            this.items = new List<long>();
             this.trueMonkey = trueMonkey;
             this.falseMonkey = falseMonkey;
             this.divisor = divisor;
             this.operation = operation;
             this.operand = operand;
             this.items.AddRange(items);
+            this.worryModifier = worryModifier;
             inspections = 0;
         }
 
-        public IEnumerable<int> InspectItems() {
+        public IEnumerable<long> InspectItems() {
             while(items.Count > 0) {
-                yield return items.RemoveAt(0);
+                long item = items[0];
+                items.RemoveAt(0);
+                yield return item;
             }
         }
 
-        public void AddItem(int item) {
+        public void AddItem(long item) {
             items.Add(item);
         }
 
-        public int MonkeyToPassItem(out int item){
+        public int MonkeyToPassItem(ref long item){
+            long curOperand = operand == -1 ? item : operand;
             inspections += 1;
-            if(operation == '*') item = item*operand;
-            else item = item + operand;
-            item = item / 3;
+            if(operation == '*') item = item*curOperand;
+            else item = item + curOperand;
+            item = item / worryModifier;
             if(item % divisor == 0) return trueMonkey;
             return falseMonkey;
         }
 
-        public int getNumInspections() {
+        public int MonkeyToPassItemB(ref long item){
+            long curOperand = operand == -1 ? item : operand;
+            inspections += 1;
+            if(operation == '*') item = item*curOperand;
+            else item = item + curOperand;
+            item = item % globalWorryModifier;
+            if(item % divisor == 0) return trueMonkey;
+            return falseMonkey;
+        }
+
+        public long getNumInspections() {
             return inspections;
         }
     }
@@ -56,78 +72,120 @@ namespace Day11
     {
         public static void Main(string[] args) {
             Part1("test.txt");
-            //Part1("input.txt");
-            //Part2("test.txt");
-            //Part2("input.txt");
+            Part1("input.txt");
+            Part2("test.txt");
+            Part2("input.txt");
 
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
         }
 
-        static List<Mokey> ReadFromFile(StringReader reader) {
+        static List<Monkey> ReadFromFile(StringReader reader, int worryModifier) {
             List<Monkey> monkeys = new List<Monkey>();
+            Monkey.globalWorryModifier = 1;
             while(reader.Peek() != -1) {
                 reader.ReadLine();
                 string[] itemStrings = reader.ReadLine().Split(':')[1].Trim().Split(new []{", "}, StringSplitOptions.RemoveEmptyEntries);
-                int[] items = new int[itemStrings.Length];
+                long[] items = new long[itemStrings.Length];
                 for (int i = 0; i < itemStrings.Length; i++) {
-                    items[i] = int.Parse(itemStrings[i]);
+                    items[i] = long.Parse(itemStrings[i]);
                 }
                 string[] operationWhole = reader.ReadLine().Split('=')[1].Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries);
                 char operation = operationWhole[1][0];
-                int operand = int.Parse(operationWhole[2]);
+                int operand;
+                if(!int.TryParse(operationWhole[2], out operand)){
+                    operand = -1;
+                }
                 int divisor = int.Parse(reader.ReadLine().Split(new []{"by"}, StringSplitOptions.RemoveEmptyEntries)[1]);
+                Monkey.globalWorryModifier *= divisor;
                 int trueMonkey = int.Parse(reader.ReadLine().Split(new []{"monkey"}, StringSplitOptions.RemoveEmptyEntries)[1]);
                 int falseMonkey = int.Parse(reader.ReadLine().Split(new []{"monkey"}, StringSplitOptions.RemoveEmptyEntries)[1]);
-                Monkey m = new Monkey(trueMonkey, falseMonkey, divisor, operation, operand, items);
+                Monkey m = new Monkey(trueMonkey, falseMonkey, divisor, operation, operand, worryModifier, items);
+                reader.ReadLine();
                 
-                monkeys.Add(monkey);
+                monkeys.Add(m);
             }
             return monkeys;
         }
 
+        static void getMaxInspections(List<Monkey> monkeys, out long mostInspections, out long most2Inspections) {
+            mostInspections = monkeys[0].getNumInspections();
+            
+            if (monkeys[1].getNumInspections() > mostInspections) {
+                most2Inspections = mostInspections;
+                mostInspections = monkeys[1].getNumInspections();
+            } else {
+                most2Inspections = monkeys[1].getNumInspections();
+            }
+            for (int i = 2; i < monkeys.Count; i += 1) {
+                long inspectionCount = monkeys[i].getNumInspections();
+                if (inspectionCount > mostInspections) {
+                    most2Inspections = mostInspections;
+                    mostInspections = inspectionCount;
+                } else if (inspectionCount > most2Inspections) {
+                    most2Inspections = inspectionCount;
+                }
+            }
+        }
+
         static void Part1(string filename) {
             using(StringReader reader = new StringReader(File.ReadAllText(filename))){
-                List<Monkey> monkeys = ReadFromFile(reader);
-                int numTurns = 1;
+                List<Monkey> monkeys = ReadFromFile(reader, 3);
+                int numTurns = 0;
 
-                while(numTurns <= 20) {
+                while(numTurns < 20) {
                     foreach(Monkey m in monkeys) {
-                        foreach(int item in monkey.InspectItems()) {
-                            int nextMonkey = monkey.MonkeyToPassItem(out item);
+                        for (var i = m.InspectItems().GetEnumerator(); i.MoveNext();) {
+                            long item = i.Current;
+                            int nextMonkey = m.MonkeyToPassItem(ref item);
                             monkeys[nextMonkey].AddItem(item);
                         }
                     }
+                    numTurns += 1;
                 }
 
-                int mostInspections = monkeys[0].getNumInspections();
-                int most2Inspections;
-                if(monkeys[1].getNumInspections() > mostInspections) {
-                    most2Inspections = mostInspections;
-                    mostInspections = monkeys[1].getNumInspections();
-                } else {
-                    most2Inspections = monkeys[1].getNumInspections();
-                }
+                long mostInspections;
+                long most2Inspections;
+                
+                getMaxInspections(monkeys, out mostInspections, out most2Inspections);
 
-                for(int i = 2; i < monkeys.Count; i += 1) {
-                    int inspectionCount = monkeys[i].getNumInspections();
-                    if(inspectionCount > mostInspections) {
-                        most2Inspections = mostInspections;
-                        mostInspections = inspectionCount;
-                    } else if (inspectionCount > most2Inspections) {
-                        most2Inspections = inspectionCount;
-                    }
-                }
 
                 Console.WriteLine("The solution to Part 1 with inputfile: {0} is: {1}", filename, mostInspections*most2Inspections);
+            }
+        }
+        
+        static void printInspectionTotals(List<Monkey> monkeys, int round){
+            Console.WriteLine("== After round {0} ==", round);
+            for (int i = 0; i < monkeys.Count; i++) {
+                Console.WriteLine("Monkey {0} inspected items {1} times.", i, monkeys[i].getNumInspections());
             }
         }
 
         static void Part2(string filename) {
             using(StringReader reader = new StringReader(File.ReadAllText(filename))){
-                // TODO: Implement Functionality Here
+                List<Monkey> monkeys = ReadFromFile(reader, 1);
+                int numTurns = 0;
 
-                Console.WriteLine("The solution to Part 2 with inputfile: {0} is: {1}", filename, "Put solution here!");
+                while(numTurns < 10000) {
+                    foreach(Monkey m in monkeys) {
+                        for (var i = m.InspectItems().GetEnumerator(); i.MoveNext();) {
+                            long item = i.Current;
+                            int nextMonkey = m.MonkeyToPassItemB(ref item);
+                            monkeys[nextMonkey].AddItem(item);
+                        }
+                    }
+                    
+                    numTurns += 1;
+                }
+                printInspectionTotals(monkeys, numTurns);
+
+                long mostInspections;
+                long most2Inspections;
+                
+                getMaxInspections(monkeys, out mostInspections, out most2Inspections);
+
+
+                Console.WriteLine("The solution to Part 2 with inputfile: {0} is: {1}", filename, mostInspections*most2Inspections);
             }
         }
     }
